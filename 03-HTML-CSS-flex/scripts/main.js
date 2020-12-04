5261a55dd9510dba303c738b8ab56b8f609dd7f0
Stefaan De Cock <stefaan.decock@hogent.be>
2020-12-04 11:56:39 +0100

    Kleine correcties.

--- 03-HTML-CSS-flex/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const centeredBoxes = document.getElementsByClassName('centered-box');
-for (let i = 0; i < centeredBoxes.length; i++) {
-    const element = centeredBoxes[i];
-    element.onclick = () => {
-        element.parentElement.remove();
-    };
-}
